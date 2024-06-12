5fb15b6f84c23af228605b4cc8f41facae0069ac
choiseongho <choiseongho@unfine.co.kr>
2024-06-12 16:10:33 +0900

    20240612 주소검색 api 구현

---
+++ src/main/java/com/ho/project/api/dto/MetaDto.java
@@ -0,0 +1,15 @@
+package com.ho.project.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MetaDto {
+
+    @JsonProperty("total_count") // 검색어에 검색된 문서 수
+    private Integer totalCount;
+}
---
+++ src/main/java/com/ho/project/api/service/KakaoAddressSearchService.java
@@ -0,0 +1,44 @@
+package com.ho.project.api.service;
+
+import com.ho.project.api.dto.KakaoApiResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class KakaoAddressSearchService {
+
+    private final RestTemplate restTemplate;
+
+    private final KakaoUriBuilderService kakaoUriBuilderService;
+
+    @Value("${kakao.rest.api.key}")
+    private String kakaoRestApiKey;
+
+
+    public KakaoApiResponseDto kakaoApiResponseDto(String address){
+        // null check
+        if(ObjectUtils.isEmpty(address)) return null;
+
+        // kakao Api 호출
+        URI uri = kakaoUriBuilderService.buildUriByAddressSearch(address);
+
+        // Header 값 Set
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.AUTHORIZATION, "KakaoAK " + kakaoRestApiKey);
+
+        HttpEntity httpEntity = new HttpEntity<>(headers);
+
+        return restTemplate.exchange(uri, HttpMethod.GET, httpEntity, KakaoApiResponseDto.class).getBody();
+    }
+}
