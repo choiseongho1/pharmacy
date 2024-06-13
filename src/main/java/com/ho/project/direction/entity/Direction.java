ddaf91c7dcc3d819b95cb6587c6ae9da33744c3f
choiseongho <choiseongho@unfine.co.kr>
2024-06-13 15:01:51 +0900

    20240612 거리 계산 알고리즘 하버사인 포뮬러 구현

---
+++ src/main/java/com/ho/project/direction/entity/Direction.java
@@ -0,0 +1,33 @@
+package com.ho.project.direction.entity;
+
+import javax.persistence.Entity;
+
+import com.ho.project.pharmacy.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity(name = "direction")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class Direction extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String inputAddress;
+    private double inputLatitude;
+    private double inputLongitude;
+
+    private String targetPharmacyName;
+    private String targetAddress;
+    private double targetLatitude;
+    private double targetLongitude;
+
+    private double distance;
+}
